e18f7bb2f82c6e739a8f11815a57947d8030b4b8
Joan Martorell <joanmartorell23@gmail.com>
2022-04-23 21:13:18 +0200

    API geolocalización (incompleta)

--- geoloc.js
+++ geoloc.js
@@ -1,5 +1,5 @@
 function iniciarMap(){
-  var coord = {lat:-34.5956145 ,lng: -58.4431949};
+  var coord = {lat:39.5695 ,lng: 2.65002};
   var map = new google.maps.Map(document.getElementById('map'),{
     zoom: 10,
     center: coord
