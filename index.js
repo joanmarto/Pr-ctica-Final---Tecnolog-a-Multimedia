99e492af44cb868ed809172e539e06b9f7eea29d
juanmeski <73904406+juanmeski@users.noreply.github.com>
2022-05-02 17:12:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,77 @@
 
-let Twit = require('twit');
 
-let T = new Twit({
-  consumer_key: 'gq7W2TPrBYz7BeXBCnzE4KoOy',
-  consumer_secret: 'S961Sp31xpKHGEEa4fBSY1gEjJTj7yT6KmWIKyjTXfAHD7NY01',
-  access_token: '1212094481308123136-ZNZwiEvFlySAAMee6pI0gvIeMvy5fU',
-  access_token_secret: 'OrNyjdw86ChmjN2pkukiPTt1YZCYX72cZJVrHKJ2BDhds',
-
-})
+const xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'museos.json', true)
+xhttp.send();
+xhttp.onreadystatechange = function () {
 
 
-var allTweets;
-var infotweet;
-function hola(){
-  T.get('search/tweets', { q: 'hola since:2011-07-11', count: 10 }, function (err, data, response) {
-    //console.log(data)
-   allTweets= data.statuses.map(tweet => printData(tweet));
-   infotweet=data;
- 
-  
-  })
-  document.getElementById("hola").innerHTML=allTweets[1].created_at;
+  if (this.readyState == 4 && this.status == 200) {
 
-}
 
-function printData(tweet) {
-  return {
-    created_at: tweet.created_at,
-    text: tweet.text,
-    name: tweet.user.name,
-    screen_name: tweet.user.screen_name,
-    profile_image_url_https: tweet.user.profile_image_url_https,
+    let museos = JSON.parse(this.responseText);
+   
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+      var distancias = [museos.length];
+      var distanciasAux = [museos.length];
+     
+
+      //funcion para calcular distancias entre ubicacion actual y museos
+      function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+        var dLon = deg2rad(lon2 - lon1);
+        var a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2)
+          ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d;
+      }
+
+      function deg2rad(deg) {
+        return deg * (Math.PI / 180)
+      }
+   //se callculan todas las distancias entre museos y ubicacion actual
+      for (var i = 0; i < museos.length; i++) {
+        var km = getDistanceFromLatLonInKm(crd.latitude, crd.longitude, museos[i]["latitude"], museos[i]["longitude"])
+       
+        distancias[i]=km;
+        distanciasAux[i]=km;
+      }
+      //ordenamos distancias
+      distancias.sort((a, b) => a - b);   
+      for (var i = 0; i < distancias.length; i++) {
+  console.log(distancias[i]);
+      }
+      for (var i = 0; i < distancias.length; i++) {
+      if(distancias[0]== distanciasAux[i]){
+
+        
+      }
+      document.getElementById("titulo_museo").innerHTML=museos[id]["name"];
+      document.getElementById("descripcion_museo").innerHTML=museos[id]["description"];
+      document.getElementById("imagen_museo").src=museos[id]["image"];
+    }
+    };
+   
+
+    function error(err) {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
   }
-
 }
-
-function gg(){
-  document.getElementById("hola").innerHTML="dsda";
-
-}
-
-
-
-
---
+++ museo.js
@@ -0,0 +1,133 @@
+//para escoger el museo en cuestion
+var id = parent.document.URL.substring(parent.document.URL.indexOf('?'), parent.document.URL.length);
+    id = id.replace("?", "");
+    console.log(id);
+
+
+const xhttp=new XMLHttpRequest();
+xhttp.open('GET','museos.json',true)
+xhttp.send();
+xhttp.onreadystatechange=function(){
+
+
+if(this.readyState==4 && this.status==200){
+    let museos=JSON.parse(this.responseText);
+  
+    //api mapa
+   
+
+        var coord = {lat:museos[id]["latitude"] ,lng: museos[id]["longitude"]};
+        var map = new google.maps.Map(document.getElementById('map'),{
+          zoom: 10,
+          center: coord
+        });
+        var marker = new google.maps.Marker({
+          position: coord,
+          map: map
+        });
+     
+   
+
+
+ 
+  //  pintar en la página web
+    console.log(museos[id]["name"]);
+    document.getElementById("titulo_museo").innerHTML=museos[id]["name"];
+    document.getElementById("descripcion_museo").innerHTML=museos[id]["description"];
+    document.getElementById("imagen_museo").src=museos[id]["image"];
+
+//api tiempo
+    window.addEventListener('load', ()=> {
+        let lon;
+        let lat;
+    
+        let temperaturaValor = document.getElementById('temperatura-valor')  
+        let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
+        
+        let ubicacion = document.getElementById('ubicacion')  
+        let iconoAnimado = document.getElementById('icono-animado') 
+    
+        let vientoVelocidad = document.getElementById('viento-velocidad') 
+    
+    
+   
+                lon=museos[id]["longitude"]
+                lat=museos[id]["latitude"]
+               console.log(lon);
+               console.log(lat);
+                //ubicación del museo    
+               const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=91eb185b92ada1500fb25d3a3f408c92`
+    
+            
+    
+               fetch(url)
+                .then( response => { return response.json()})
+                .then( data => {
+                    //console.log(data)
+                    
+                    let temp = Math.round(data.main.temp)
+                    //console.log(temp)
+                    temperaturaValor.textContent = `${temp-273,15} ° C`
+    
+                    //console.log(data.weather[0].description)
+                    let desc = data.weather[0].description
+                    temperaturaDescripcion.textContent = desc.toUpperCase()
+                    ubicacion.textContent = data.name
+                    
+                    vientoVelocidad.textContent = `${data.wind.speed} m/s`
+                    
+                  
+    
+                    //para iconos dinámicos
+                    console.log(data.weather[0].main)
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                          iconoAnimado.src='animated/thunder.svg'
+                          console.log('TORMENTA');
+                          break;
+                        case 'Drizzle':
+                          iconoAnimado.src='animated/rainy-2.svg'
+                          console.log('LLOVIZNA');
+                          break;
+                        case 'Rain':
+                          iconoAnimado.src='animated/rainy-7.svg'
+                          console.log('LLUVIA');
+                          break;
+                        case 'Snow':
+                          iconoAnimado.src='animated/snowy-6.svg'
+                            console.log('NIEVE');
+                          break;                        
+                        case 'Clear':
+                            iconoAnimado.src='animated/day.svg'
+                            console.log('LIMPIO');
+                          break;
+                        case 'Atmosphere':
+                          iconoAnimado.src='animated/weather.svg'
+                            console.log('ATMOSFERA');
+                            break;  
+                        case 'Clouds':
+                            iconoAnimado.src='animated/cloudy-day-1.svg'
+                            console.log('NUBES');
+                            break;  
+                        default:
+                          iconoAnimado.src='animated/cloudy-day-1.svg'
+                          console.log('por defecto');
+                      }
+    
+                })
+                .catch( error => {
+                    console.log(error)
+                })
+           })
+              
+     
+}
+
+
+}
+
+
+
+  
+   
+
