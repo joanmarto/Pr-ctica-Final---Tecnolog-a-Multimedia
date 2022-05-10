574072c5596bec81f3739fae8194eed75d8dd7fa
Joan Martorell <joanmartorell23@gmail.com>
2022-05-10 19:07:42 +0200

    Markador de usuario añadido

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ xhttp.onreadystatechange = function () {
 
     navigator.geolocation.getCurrentPosition(success, error, options);
 
-    //api google maps para mostrar la posición de los museos
+    //API google maps para mostrar la posición de los museos
     var coord = [];
     for (var i = 0; i < museos.length; i++) {
       //Obtenemos todas las coordenadas
@@ -68,24 +68,22 @@ xhttp.onreadystatechange = function () {
       console.log("Museo " + museos[i]["name"] + " -> lat: " + coord[i].lat + ", long: " + coord[i].lng);
     }
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };})
     //Creamos el mapa
+    var uib = {lat: 39.637464, lng: 2.644435} //Coordenadas uib
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
+      /*
       center: navigator.geolocation.getCurrentPosition(
         (position) => {
           const pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };})  //Posición del cliente
+          */
+      center: uib
     });
 
-    //Añadimos el mapa
+    //Añadimos los marcadores de los museos
     var marker = [];
     for(var i = 0; i < museos.length; i++){
       marker[i] = new google.maps.Marker({
@@ -94,6 +92,22 @@ xhttp.onreadystatechange = function () {
         title: museos[i]["name"]
       });
     }
+    //Añadimos el marcador del usuario. Para diferenciarlo del resto será un icono svg de color azul
+    const svgMarker = {
+      path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+      fillColor: "blue",
+      fillOpacity: 0.6,
+      strokeWeight: 0,
+      rotation: 0,
+      scale: 2,
+      anchor: new google.maps.Point(15, 30),
+    };
+    var userMarker = new google.maps.Marker({
+      position: uib,
+      map: map,
+      title: "you",
+      icon: svgMarker
+    });
   }
 }
 
