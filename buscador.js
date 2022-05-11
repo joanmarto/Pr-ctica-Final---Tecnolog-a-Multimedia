1ddb7290042aca8c591cf16d4838f5f330b4d8f5
Joan Martorell <73879205+joanmarto@users.noreply.github.com>
2022-05-11 16:02:36 +0200

    Update buscador.js

--- buscador.js
+++ buscador.js
@@ -106,11 +106,11 @@ function autocomplete(inp, arr) {
         } else {
             //Cargamos la página del museo seleccionado
             let url = `/museo.html?${pos}`;
-            alert(url);
-            alert(window.location.href);
+            alert("URL: " + url);
+            alert("location.href: " + window.location.href);
             //location.assign(url);
             //window.location.href = url;
-            window.location.assign(url);
+            window.location.assign(`/museo.html?${pos}`);
         }
         return false;
     });
