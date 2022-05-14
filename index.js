a6cac7d1b0524ce28d7b78b0ba1d08eac0087a65
Joan Martorell <joanmartorell23@gmail.com>
2022-05-14 14:47:09 +0200

    Api google maps arreglada

--- index.js
+++ index.js
@@ -4,111 +4,112 @@ id = id.replace("?", "");
 id = id.replace("#", "");
 console.log(id);
 
+var museos;
+
+var userLat, userLng; //Coordenadas cliente
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 const xhttp = new XMLHttpRequest();
 xhttp.open('GET', 'museos.json', true)
 xhttp.send();
 xhttp.onreadystatechange = function () {
 
-
   if (this.readyState == 4 && this.status == 200) {
 
-    let museos = JSON.parse(this.responseText);
-
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-
-    var crd;
-    function success(pos) {
-      crd = pos.coords;
-      var distancias = [museos.length];
-      var distanciasAux = [museos.length];
-
-      //se callculan todas las distancias entre museos y ubicacion actual
-      for (var i = 0; i < museos.length; i++) {
-        var km = getDistanceFromLatLonInKm(crd.latitude, crd.longitude, museos[i]["latitude"], museos[i]["longitude"])
-        distancias[i] = km;
-        distanciasAux[i] = km;
-      }
-
-      //ordenamos distancias
-      distancias.sort((a, b) => a - b);
-      for (var i = 0; i < distancias.length; i++) {
-        console.log(distancias[i]);
-      }
-
-      for (var i = 0; i < distanciasAux.length; i++) {
-        console.log(distanciasAux[i]);
-      }
-
-      for (var x = 0; x < 4; x++) {
-        for (var i = 0; i < distancias.length; i++) {
-          if (distancias[x] == distanciasAux[i]) {
-            document.getElementById("titulo_galeria_index" + x.toString()).innerHTML = museos[i]["name"];
-            document.getElementById("foto_galeria_index" + x.toString()).src = museos[i]["image"];
-            document.getElementById("titulo_galeria_index" + x.toString()).href = "museo.html?" + i.toString();
-          }
-        }
-      }
-    }
-
-    function error(err) {
-      console.warn('ERROR(' + err.code + '): ' + err.message);
-    };
-
+    museos = JSON.parse(this.responseText);
     navigator.geolocation.getCurrentPosition(success, error, options);
+    //Esperamos a que se obtenga la ubicación
+    setTimeout(addClosermuseums, 1000);
+    setTimeout(addMap, 1000);
+  }
+}
 
-    //API google maps para mostrar la posición de los museos
-    var coord = [];
-    for (var i = 0; i < museos.length; i++) {
-      //Obtenemos todas las coordenadas
-      coord[i] = { lat: Number(museos[i]["latitude"]), lng: Number(museos[i]["longitude"]) };
-      console.log("Museo " + museos[i]["name"] + " -> lat: " + coord[i].lat + ", long: " + coord[i].lng);
+function success(pos) {
+  var crd = pos.coords;
+  userLat = crd.latitude;
+  userLng = crd.longitude;
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+//Función para mostrar los museos más cercanos
+function addClosermuseums(){
+  var distancias = [museos.length];
+  var distanciasAux = [museos.length];
+  //se calculan todas las distancias entre museos y ubicacion actual
+  for (var i = 0; i < museos.length; i++) {
+    var km = getDistanceFromLatLonInKm(userLat, userLng, museos[i]["latitude"], museos[i]["longitude"])
+    distancias[i] = km;
+    distanciasAux[i] = km;
+  }
+
+  //ordenamos distancias
+  distancias.sort((a, b) => a - b);
+  for (var i = 0; i < distancias.length; i++) {
+    console.log(distancias[i]);
+  }
+
+  for (var i = 0; i < distanciasAux.length; i++) {
+    console.log(distanciasAux[i]);
+  }
+
+  for (var x = 0; x < 4; x++) {
+    for (var i = 0; i < distancias.length; i++) {
+      if (distancias[x] == distanciasAux[i]) {
+        document.getElementById("titulo_galeria_index" + x.toString()).innerHTML = museos[i]["name"];
+        document.getElementById("foto_galeria_index" + x.toString()).src = museos[i]["image"];
+        document.getElementById("titulo_galeria_index" + x.toString()).href = "museo.html?" + i.toString();
+      }
     }
+  }
+}
 
-    //Creamos el mapa
-    var uib = { lat: 39.637464, lng: 2.644435 } //Coordenadas uib
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      /*
-      center: navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };})  //Posición del cliente
-          */
-      center: uib
-    });
-
-    //Añadimos los marcadores de los museos
-    var marker = [];
-    for (var i = 0; i < museos.length; i++) {
-      marker[i] = new google.maps.Marker({
-        position: coord[i],
-        map: map,
-        title: museos[i]["name"]
-      });
-    }
-    //Añadimos el marcador del usuario. Para diferenciarlo del resto será un icono svg de color azul
-    const svgMarker = {
-      path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-      fillColor: "blue",
-      fillOpacity: 0.6,
-      strokeWeight: 0,
-      rotation: 0,
-      scale: 2,
-      anchor: new google.maps.Point(15, 30),
-    };
-    var userMarker = new google.maps.Marker({
-      position: uib,
+//API google maps para mostrar la posición de los museos
+function addMap(){
+  var coord = [];
+  for (var i = 0; i < museos.length; i++) {
+    //Obtenemos todas las coordenadas
+    coord[i] = { lat: Number(museos[i]["latitude"]), lng: Number(museos[i]["longitude"]) };
+    //console.log("Museo " + museos[i]["name"] + " -> lat: " + coord[i].lat + ", long: " + coord[i].lng);
+  }
+  
+  //Creamos el mapa
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: Number(userLat), lng: Number(userLng)}
+  });
+  
+  //Añadimos los marcadores de los museos
+  var marker = [];
+  for (var i = 0; i < museos.length; i++) {
+    marker[i] = new google.maps.Marker({
+      position: coord[i],
       map: map,
-      title: "you",
-      icon: svgMarker
+      title: museos[i]["name"]
     });
   }
+  //Añadimos el marcador del usuario. Para diferenciarlo del resto será un icono svg de color azul
+  const svgMarker = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "blue",
+    fillOpacity: 0.6,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,
+    anchor: new google.maps.Point(15, 30),
+  };
+  var userMarker = new google.maps.Marker({
+    position: {lat: Number(userLat), lng: Number(userLng)},
+    map: map,
+    title: "you",
+    icon: svgMarker
+  });
 }
 
 //funcion para calcular distancias entre ubicacion actual y museos
--- museo.js
+++ museo.js
@@ -3,147 +3,130 @@ var id = parent.document.URL.substring(parent.document.URL.indexOf('?'), parent.
 id = id.replace("?", "");
 console.log(id);
 
-let museos;
+var museos;
 
 const xhttp = new XMLHttpRequest();
 xhttp.open('GET', 'museos.json', true)
 xhttp.send();
 xhttp.onreadystatechange = function () {
 
-
   if (this.readyState == 4 && this.status == 200) {
     museos = JSON.parse(this.responseText);
 
     //api google maps para mostrar la posición del museo
-   var coord = { lat: Number(museos[id]["latitude"]), lng: Number(museos[id]["longitude"]) };
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+    addMap();
 
     // pintar en la página web
-    console.log(museos[id]["name"]);
-    document.getElementById("titulo_museo").innerHTML = museos[id]["name"];
-    document.getElementById("descripcion_museo").innerHTML = museos[id]["description"];
-    document.getElementById("imagen_museo").src = museos[id]["image"];
+    printWebPage();
+  }
+}
 
-    document.getElementById("direccion").innerHTML = museos[id]["address"];
-    document.getElementById("apertura").innerHTML = museos[id]["openingHoursSpecification"];
-    document.getElementById("telefono").innerHTML = museos[id]["telephone"];
-    document.getElementById("email").innerHTML = museos[id]["email"];
-    document.getElementById("web").innerHTML = museos[id]["sameAs"];
-    document.getElementById("video").src=museos[id]["url"];
+function addMap() {
+  //Coordenadas del museo
+  var coord = { lat: Number(museos[id]["latitude"]), lng: Number(museos[id]["longitude"]) };
+  //Creamos el mapa
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: coord
+  });
+  //Añadimos un marcador
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
 
-    //api tiempo
-    window.addEventListener('load', () => {
-      let lon;
-      let lat;
+function printWebPage() {
+  //console.log(museos[id]["name"]);
+  document.getElementById("titulo_museo").innerHTML = museos[id]["name"];
+  document.getElementById("descripcion_museo").innerHTML = museos[id]["description"];
+  document.getElementById("imagen_museo").src = museos[id]["image"];
 
-      let temperaturaValor = document.getElementById('temperatura-valor')
-      let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
+  document.getElementById("direccion").innerHTML = museos[id]["address"];
+  document.getElementById("apertura").innerHTML = museos[id]["openingHoursSpecification"];
+  document.getElementById("telefono").innerHTML = museos[id]["telephone"];
+  document.getElementById("email").innerHTML = museos[id]["email"];
+  document.getElementById("web").innerHTML = museos[id]["sameAs"];
+  document.getElementById("video").src = museos[id]["url"];
+}
 
-      let ubicacion = document.getElementById('ubicacion')
-      let iconoAnimado = document.getElementById('icono-animado')
+//API del tiempo
+window.addEventListener('load', () => {
 
-      let vientoVelocidad = document.getElementById('viento-velocidad')
+  let temperaturaValor = document.getElementById('temperatura-valor')
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
 
+  let ubicacion = document.getElementById('ubicacion')
+  let iconoAnimado = document.getElementById('icono-animado')
 
+  let vientoVelocidad = document.getElementById('viento-velocidad')
 
-      lon = museos[id]["longitude"]
-      lat = museos[id]["latitude"]
-      
-      //ubicación del museo    
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=91eb185b92ada1500fb25d3a3f408c92`
+  let lat = museos[id]["latitude"]
+  let lon = museos[id]["longitude"]
+  //Lamada a la api con la ubicación del museo    
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=91eb185b92ada1500fb25d3a3f408c92`
 
-      fetch(url)
-        .then(response => { return response.json() })
-        .then(data => {
-          //console.log(data)
+  fetch(url)
+    .then(response => { return response.json() })
+    .then(data => {
+      //console.log(data)
 
-          let temp = Math.round(data.main.temp)
-          //console.log(temp)
-          temperaturaValor.textContent = `${temp - 273, 15} ° C`
+      let temp = Math.round((data.main.temp - 273))
+      console.log(temp)
+      temperaturaValor.textContent = `${temp} °C`
 
-          //console.log(data.weather[0].description)
-          let desc = data.weather[0].description
-         // temperaturaDescripcion.textContent = desc.toUpperCase()
-          ubicacion.textContent = data.name
+      //console.log(data.weather[0].description)
+      let desc = data.weather[0].description
+      // temperaturaDescripcion.textContent = desc.toUpperCase()
+      ubicacion.textContent = data.name
 
-          vientoVelocidad.textContent = `${data.wind.speed} m/s`
+      vientoVelocidad.textContent = `${data.wind.speed} m/s`
 
+      //iconos dinámicos
+      switch (data.weather[0].main) {
+        case 'Thunderstorm':
+          iconoAnimado.src = 'animated/thunder.svg'
+          //console.log('TORMENTA');
+          temperaturaDescripcion.textContent = 'TORMENTA';
+          break;
+        case 'Drizzle':
+          iconoAnimado.src = 'animated/rainy-2.svg'
+          //console.log('LLOVIZNA');
+          temperaturaDescripcion.textContent = 'LLOVIZNA';
+          break;
+        case 'Rain':
+          iconoAnimado.src = 'animated/rainy-7.svg'
+          //console.log('LLUVIA');
+          temperaturaDescripcion.textContent = 'LLUVIA';
+          break;
+        case 'Snow':
+          iconoAnimado.src = 'animated/snowy-6.svg'
+          //console.log('NIEVE');
+          temperaturaDescripcion.textContent = 'NIEVE';
+          break;
+        case 'Clear':
+          iconoAnimado.src = 'animated/day.svg'
+          //console.log('LIMPIO');
+          temperaturaDescripcion.textContent = 'DESPEJADO';
+          break;
+        case 'Atmosphere':
+          iconoAnimado.src = 'animated/weather.svg'
+          //console.log('ATMOSFERA');
+          temperaturaDescripcion.textContent = 'VIENTO';
+          break;
+        case 'Clouds':
+          iconoAnimado.src = 'animated/cloudy-day-1.svg'
+          //console.log('NUBES');
+          temperaturaDescripcion.textContent = 'NUBES';
+          break;
+        default:
+          iconoAnimado.src = 'animated/cloudy-day-1.svg'
+          //console.log('por defecto');
+          temperaturaDescripcion.textContent = 'NUBES';
+      }
 
-
-          //para iconos dinámicos
-          console.log(data.weather[0].main)
-          switch (data.weather[0].main) {
-            case 'Thunderstorm':
-              iconoAnimado.src = 'animated/thunder.svg'
-              //console.log('TORMENTA');
-              temperaturaDescripcion.textContent = 'TORMENTA';
-              break;
-            case 'Drizzle':
-              iconoAnimado.src = 'animated/rainy-2.svg'
-              console.log('LLOVIZNA');
-              temperaturaDescripcion.textContent = 'LLOVIZNA';
-              break;
-            case 'Rain':
-              iconoAnimado.src = 'animated/rainy-7.svg'
-              console.log('LLUVIA');
-              temperaturaDescripcion.textContent = 'LLUVIA';
-              break;
-            case 'Snow':
-              iconoAnimado.src = 'animated/snowy-6.svg'
-              console.log('NIEVE');
-              temperaturaDescripcion.textContent = 'NIEVE';
-              break;
-            case 'Clear':
-              iconoAnimado.src = 'animated/day.svg'
-              console.log('LIMPIO');
-              temperaturaDescripcion.textContent = 'DESPEJADO';
-              break;
-            case 'Atmosphere':
-              iconoAnimado.src = 'animated/weather.svg'
-              console.log('ATMOSFERA');
-              temperaturaDescripcion.textContent = 'VIENTO';
-              break;
-            case 'Clouds':
-              iconoAnimado.src = 'animated/cloudy-day-1.svg'
-              console.log('NUBES');
-              temperaturaDescripcion.textContent = 'NUBES';
-              break;
-            default:
-              iconoAnimado.src = 'animated/cloudy-day-1.svg'
-              console.log('por defecto');
-              temperaturaDescripcion.textContent = 'NUBES';
-          }
-
-        })
-        .catch(error => {
-          console.log(error)
-        })
     })
-
-
-  }
-
-
-}
-
-//Buscador de museos
-/*
-const formulario = document.querySelector('#formulario');
-const search = document.querySelector('#search');
-
-const buscar = ()=>{
-  //console.log(buscador.value);
-  //const text = formulario.ariaValueMax.toLowerCase();
-  for (let museo of museos){
-    console.log(museo);
-  }
-}
-
-search.addEventListener('click', buscar)*/
+    .catch(error => {
+      console.log(error)
+    })
+})
