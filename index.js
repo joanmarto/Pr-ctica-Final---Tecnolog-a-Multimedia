c7e1ac10f3e470773a8c7aca046cbf475151cf47
juanmeski <juanmesquida0303@gmail.com>
2022-05-18 01:47:32 +0200

    por fin juanmeski tiene git

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//para escoger el museo en cuestión  asddds
+//para escoger el museo en cuestión
 var id = parent.document.URL.substring(parent.document.URL.indexOf('?'), parent.document.URL.length);
 id = id.replace("?", "");
 id = id.replace("#", "");
